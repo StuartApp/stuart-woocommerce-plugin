9714d5c5174bf15632f1162abdfbda9351e11937
Jose Hervás Díaz <jhervasdiaz@gmail.com>
2022-12-20 13:32:16 +0100

    Added forked on

--- stuart-woocommerce/custom-shipping-method.php
+++ stuart-woocommerce/custom-shipping-method.php
@@ -3,7 +3,9 @@
     Plugin Name: Stuart Delivery Integration for WooCommerce
     Plugin URI: http://plugins.stuart-apps.solutions/wordpress/
     Description: Integrate Stuart Delivery into your WooCommerce site
-    Author: Jose Hervas Diaz <ji.hervas@stuart.com> (extended originally from Malttt's WooCommerce Plugin)
+    Author: Jose Hervas Diaz <ji.hervas@stuart.com>
+    Original Author: Malttt
+    Forked on: 2020-10-10
     Version: 1.0.1
     License : GPL
     Text Domain: stuart-delivery
--- stuart-woocommerce/stuart-woocommerce.php
+++ stuart-woocommerce/stuart-woocommerce.php
@@ -4,6 +4,8 @@
     Plugin URI: http://plugins.stuart-apps.solutions/wordpress/
     Description: Integrate Stuart Delivery into your WooCommerce site
     Author: Jose Hervas Diaz <ji.hervas@stuart.com> (extended originally from Malttt's WooCommerce Plugin)
+    Original Author: Malttt
+    Forked on: 2020-10-10
     Version: 1.0.1
     License : GPL
     Text Domain: stuart-delivery
